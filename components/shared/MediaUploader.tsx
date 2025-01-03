b56309870a472d3d3f4ce81462813c6927fb71f4
atomar1 <ansh06tomar@gmail.com>
2025-01-02 16:38:57 -0800

    updated webhooks and api keys

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="ansh_pixify"
       options={{
         multiple: false,
         resourceType: "image",
