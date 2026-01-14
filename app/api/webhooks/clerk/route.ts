584d0de9bd8344d2a51a4118f8806e33581c753c
Ansh Tomar <89928686+atomar1@users.noreply.github.com>
2026-01-13 22:53:45 -0800

    Handle optional fields in user creation and update
    Updated user object creation to handle optional fields.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -61,14 +61,14 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
+  const user = {
+    clerkId: id,
+    email: email_addresses[0].email_address,
+    username: username ?? "",
+    firstName: first_name ?? "",
+    lastName: last_name ?? "",
+    photo: image_url,
+  };
 
     const newUser = await createUser(user);
 
@@ -89,11 +89,12 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
-      photo: image_url,
-    };
+    firstName: first_name ?? "",
+    lastName: last_name ?? "",
+    username: username ?? "",
+    photo: image_url,
+  };
+
 
     const updatedUser = await updateUser(id, user);
 
